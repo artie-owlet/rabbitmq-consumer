1008c321a879b41b97d86e1875666feb377f739a
artie-owlet <artie-owlet@protonmail.com>
2021-08-07 16:28:42 +0300

    fix TopicPattern; add test for TopicPattern

--- src/topic-pattern.ts
+++ src/topic-pattern.ts
@@ -19,8 +19,15 @@ export class TopicPattern {
 
     public match(rk: string): boolean {
         const rkWords = rk.split('.');
-        let rkwId = 0;
-        for (let fpId = 0; fpId < this.fixedPatterns.length - 1; ++fpId) {
+        if (!this.matchFixed(this.fixedPatterns[0], rkWords)) {
+            return false;
+        }
+        if (this.fixedPatterns.length === 0) {
+            return this.fixedPatterns[0].length === rkWords.length;
+        }
+
+        let rkwId = this.fixedPatterns[0].length;
+        for (let fpId = 1; fpId < this.fixedPatterns.length - 1; ++fpId) {
             while (this.fixedPatterns[fpId].length <= rkWords.length - rkwId &&
                 !this.matchFixed(this.fixedPatterns[fpId], rkWords.slice(rkwId))) {
                 ++rkwId;
@@ -30,15 +37,16 @@ export class TopicPattern {
             }
             rkwId += this.fixedPatterns[fpId].length;
         }
+
         const lastPattern = this.fixedPatterns[this.fixedPatterns.length - 1];
-        if (lastPattern.length > rkWords.length - rkwId) {
-            return false;
-        }
         rkwId = rkWords.length - lastPattern.length;
         return this.matchFixed(lastPattern, rkWords.slice(rkwId));
     }
 
     private matchFixed(pWords: string[], rkWords: string[]): boolean {
+        if (pWords.length > rkWords.length) {
+            return false;
+        }
         for (let i = 0; i < pWords.length; ++i) {
             if (pWords[i] === '*') {
                 continue;
--- test/match-topic.ts
+++ test/match-topic.ts
@@ -2,30 +2,43 @@ import { expect } from 'chai';
 
 import { TopicPattern } from '../src/topic-pattern';
 
-describe('TopicPattern.match', () => {
-    it('should match', () => {
-        expect(new TopicPattern('abc').match('abc')).equal(true);
-        expect(new TopicPattern('*.b.c').match('a.b.c')).equal(true);
-        expect(new TopicPattern('a.*.c').match('a.b.c')).equal(true);
-        expect(new TopicPattern('a.b.*').match('a.b.c')).equal(true);
+describe('TopicPattern', () => {
+    describe('match', () => {
+        it('*', () => {
+            expect(new TopicPattern('*.b.c').match('a.b.c')).equal(true);
+            expect(new TopicPattern('a.*.c').match('a.b.c')).equal(true);
+            expect(new TopicPattern('a.b.*').match('a.b.c')).equal(true);
 
-        expect(new TopicPattern('a.#.c').match('a.c')).equal(true);
-        expect(new TopicPattern('a.#.c').match('a.b.c')).equal(true);
-        expect(new TopicPattern('a.#.c').match('a.b.b.c')).equal(true);
+            expect(new TopicPattern('*.b.c').match('b.c')).equal(false);
+            expect(new TopicPattern('*.b.c').match('a.a.b.c')).equal(false);
+            expect(new TopicPattern('a.*.c').match('a.c')).equal(false);
+            expect(new TopicPattern('a.*.c').match('a.b.b.c')).equal(false);
+            expect(new TopicPattern('a.b.*').match('a.b')).equal(false);
+            expect(new TopicPattern('a.b.*').match('a.b.c.c')).equal(false);
+        });
 
-        expect(new TopicPattern('#.b.c').match('b.c')).equal(true);
-        expect(new TopicPattern('#.b.c').match('a.b.c')).equal(true);
-        expect(new TopicPattern('#.b.c').match('a.a.b.c')).equal(true);
+        it('#', () => {
+            expect(new TopicPattern('#.b.c').match('b.c')).equal(true);
+            expect(new TopicPattern('#.b.c').match('a.b.c')).equal(true);
+            expect(new TopicPattern('#.b.c').match('a.a.b.c')).equal(true);
 
-        expect(new TopicPattern('a.b.#').match('a.b')).equal(true);
-        expect(new TopicPattern('a.b.#').match('a.b.c')).equal(true);
-        expect(new TopicPattern('a.b.#').match('a.b.c.c')).equal(true);
+            expect(new TopicPattern('a.#.c').match('a.c')).equal(true);
+            expect(new TopicPattern('a.#.c').match('a.b.c')).equal(true);
+            expect(new TopicPattern('a.#.c').match('a.b.b.c')).equal(true);
 
-        expect(new TopicPattern('a.#.c.#.e').match('a.c.e')).equal(true);
-        expect(new TopicPattern('a.#.c.#.e').match('a.c.d.e')).equal(true);
-        expect(new TopicPattern('a.#.c.#.e').match('a.c.d.d.e')).equal(true);
-        expect(new TopicPattern('a.#.c.#.e').match('a.b.c.e')).equal(true);
-        expect(new TopicPattern('a.#.c.#.e').match('a.b.c.d.e')).equal(true);
-        expect(new TopicPattern('a.#.c.#.e').match('a.b.c.d.d.e')).equal(true);
+            expect(new TopicPattern('a.b.#').match('a.b')).equal(true);
+            expect(new TopicPattern('a.b.#').match('a.b.c')).equal(true);
+            expect(new TopicPattern('a.b.#').match('a.b.c.c')).equal(true);
+        });
+
+        it('multiple #', () => {
+            expect(new TopicPattern('a.#.c.#.e').match('a.c.e')).equal(true);
+            expect(new TopicPattern('a.#.c.#.e').match('a.c.d.e')).equal(true);
+            expect(new TopicPattern('a.#.c.#.e').match('a.c.d.d.e')).equal(true);
+            expect(new TopicPattern('a.#.c.#.e').match('a.b.c.e')).equal(true);
+            expect(new TopicPattern('a.#.c.#.e').match('a.b.c.d.e')).equal(true);
+            expect(new TopicPattern('a.#.c.#.e').match('a.b.c.d.d.e')).equal(true);
+            expect(new TopicPattern('a.#.c.#.e').match('a.b.d.e')).equal(false);
+        });
     });
 });
